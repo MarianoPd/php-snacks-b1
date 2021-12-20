17c64ee357e16bef9ca46aff101493f3ceedb0a5
MarianoPd <marianopidone@gmail.com>
2021-12-20 15:44:07 +0100

    Snack 4

---
+++ Snak 4/index.php
@@ -0,0 +1,39 @@
+<?php
+    $nElement = 15;
+    $min = $_GET['min'];
+    $max = $_GET['max'];
+    function creteRandArray($min, $max,$nElement){
+        $newArray = [];
+        if(($max - $min) <$nElement) $nElement = ($max - $min + 1);
+        while(count($newArray) < $nElement){
+            $number = rand($min, $max);
+            if(!in_array($number, $newArray)){
+                $newArray[] = $number;
+            }
+        }
+        return $newArray;
+    };
+
+    function printNumArray($array){
+        echo "<h2>Array</h2>";
+        for($i = 0; $i < count($array); $i++){
+            echo "<span>$array[$i]    </span>";
+        }
+    };
+
+    $array = creteRandArray($min, $max,$nElement);
+    printNumArray($array);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 4</title>
+</head>
+<body>
+    
+</body>
+</html>
